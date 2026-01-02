f38bb9d30ff698f22f88eeab62078e24357963dd
jacobthomas1189-sys <jacobthomas1189@gmail.com>
2026-01-02 18:43:09 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,33 @@
-const CACHE_NAME = "currency-tool-v2";
+const CACHE_NAME = "currency-tool-v7";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./manifest.json",
+                "./icon-192.png",
+                "./icon-512.png"
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            )
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(res => res || fetch(event.request))
+    );
 });
